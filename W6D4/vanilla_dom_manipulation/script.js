b387dc5c811e673248c0b661c2e11a222088aa34
Jimmy Li <jimmyli921@gmail.com>
2017-09-06 20:42:29 -0700

    Finished W6D4 HW

--- W6D4/vanilla_dom_manipulation/script.js
+++ W6D4/vanilla_dom_manipulation/script.js
@@ -16,16 +16,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+
   // adding SF places as list items
-
   // --- your code here!
+  const handleFavSubmit = (e) => {
+    event.preventDefault();
+    const ul = document.getElementById("sf-places");
+    const li = document.createElement("li");
+    const input = document.querySelector(".favorite-input");
+    li.textContent = input.value;
+    ul.appendChild(li);
+    input.value = "";
+  };
 
+  const button = document.querySelector(".favorite-submit");
+  button.addEventListener("click", handleFavSubmit);
 
 
   // adding new photos
-
   // --- your code here!
+  const photoForm = document.querySelector(".photo-form-container");
+  const photoShowButton = document.querySelector(".photo-show-button");
+  photoShowButton.addEventListener("click", event => {
+    if(photoForm.className === "photo-form-container hidden") {
+      photoForm.className = "photo-form-container";
+    } else {
+      photoForm.className = "photo-form-container hidden";
+    }
+  });
 
-
+  const photoSubmitButton = document.querySelector(".photo-url-submit");
+  photoSubmitButton.addEventListener("click", event => {
+    event.preventDefault();
+    const photoUrlInput = document.querySelector(".photo-url-input");
+    const dogUl = document.querySelector(".dog-photos");
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.src = photoUrlInput.value;
+    li.appendChild(img);
+    dogUl.appendChild(li);
+    photoUrlInput.value = "";
+  });
 
 });
